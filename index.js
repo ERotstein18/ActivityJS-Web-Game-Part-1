15177fa37f5fb5632c719b3160c32f546425f275
Eyrma <erotstein@outlook,com>
2023-08-17 21:37:14 -0700

    First try to code

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+function newImage
 let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
+greenCharacter.src = 'assets/pine-tree.png'
 greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
+greenCharacter.style.left = '400px'
+greenCharacter.style.bottom = '200px'
 document.body.append(greenCharacter)
+
+
+//* let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter) 
+
+//Adding a Pine-tree image //
+let greenCharacter = document.createElement('img')
+greenCharacter.src = 'assets/pine-tree.png'
+greenCharacter.style.position = 'fixed'
+greenCharacter.style.left = '400px'
+greenCharacter.style.bottom = '200px'
+document.body.append(greenCharacter)
+
